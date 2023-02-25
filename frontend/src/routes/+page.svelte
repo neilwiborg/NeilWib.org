bef1f29cfc3097126772f35d02302dfcc1202a4c
Neil Wiborg <NeilWiborg@outlook.com>
2023-02-25 00:32:14 -0800

    try to protect program4 route

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,5 +2,4 @@
 </script>
 
 <h1>Neil Wiborg</h1>
-<p>Visit <a href="/sst">Safwaan Standard Time here</a>
-    , or <a href="/program4">click here for CSS 436 Program 4</a></p>
+<p>Visit <a href="/sst">Safwaan Standard Time here</a>, or <a data-sveltekit-reload href="/program4">click here for CSS 436 Program 4</a></p>
