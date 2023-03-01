c777b1815d97e29725dff828a376455ccebb2f43
Neil Wiborg <NeilWiborg@outlook.com>
2023-02-28 21:03:29 -0800

    frontend changes

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <div class="hero">
 	<nav class="container-fluid">
 		<a href="/about" class="contrast">About</a>
-		<details role="list" dir="rtl" class="contrast">
-			<summary aria-haspopup="listbox" role="link">Projects</summary>
+		<details role="list" dir="rtl">
+			<summary aria-haspopup="listbox" role="link" class="contrast">Projects</summary>
 			<ul role="listbox">
 				<li><a href="/sst">Safwaan Standard Time</a></li>
 				<li><a data-sveltekit-reload href="/program4">CSS 436 Program 4</a></li>
@@ -16,7 +16,7 @@
 		</hgroup>
 		<br>
 	</header>
-	<br>
 </div>
+<br>
 
 <slot />
--- frontend/src/routes/program4/+page.svelte
+++ frontend/src/routes/program4/+page.svelte
@@ -15,18 +15,23 @@
 	let operationRunning = false;
 
 	const onLoad = () => {
+		loadDataLoading = true;
 		clearDisabled = false;
 		errorText = 'Data loaded successfully!';
+		loadDataLoading = false;
 	};
 
 	const onClear = () => {
+		clearDataLoading = true;
 		firstName = '';
 		lastName = '';
 		clearDisabled = true;
 		errorText = 'Data cleared';
+		clearDataLoading = false;
 	};
 
 	const onSubmit = () => {
+		queryLoading = true;
 		// if the user has not entered either a first or last name
 		if (firstName === '' && lastName === '') {
 			// TODO: change small text color to red
@@ -41,7 +46,6 @@
 				errorText = 'Results for ' + lastName + ':';
 			}
 		}
-		queryLoading = true;
 		fetch(import.meta.env.VITE_BACKEND_HOSTNAME + "/program4/loadData")
 		.then((response) => response.text()
 		.then((data) => {
@@ -56,62 +60,64 @@
 </svelte:head>
 
 <main class="container">
-	<h2>CSS 436 Program 4</h2>
-	<form>
-		<div class="grid">
-			{#if loadDataLoading}
-			<button type="button" class="secondary" aria-busy={queryLoading ? "true" : "false"}>Please wait...</button>
-			{:else}
-			<button type="button" class="secondary" on:click={onLoad}
-			disabled={(loadDataLoading || clearDataLoading || queryLoading) ? true : false}>Load Data</button>
-			{/if}
-			{#if clearDataLoading}
-			<button type="reset" class="destructive" aria-busy={queryLoading ? "true" : "false"}>Please wait...</button>
-			{:else}
-				<button type="reset" class="destructive" disabled={clearDisabled} on:click={onClear}
-					>Clear Data</button>
-			{/if}
-		</div>
-		<fieldset>
+	<article>
+		<h2>CSS 436 Program 4</h2>
+		<form>
 			<div class="grid">
-				<label>
-					First Name
-					<input type="text" placeholder="Enter a first name" bind:value={firstName} />
-				</label>
-				<label>
-					Last Name
-					<input type="text" placeholder="Enter a last name" bind:value={lastName} />
-				</label>
-			</div>
-			<small>At least one field must be entered</small>
-			{#if queryLoading}
-			<button type="submit" aria-busy={queryLoading ? "true" : "false"}>Please wait...</button>
-			{:else}
-			<button type="submit" on:click={onSubmit} disabled={(loadDataLoading || clearDataLoading || queryLoading) ? true : false}>Query</button>
-			{/if}
-		</fieldset>
-		<div class="container">
-			<h3>Results</h3>
-			<output>
-				{#if resultsText.length > 0}
-					{#each JSON.parse(resultsText).users as user}
-						<details>
-							<summary>
-								{user.firstName} {user.lastName}
-							</summary>
-							<ul>
-								{#each Object.entries(user) as [attr, val]}
-									{#if attr !== "firstName" && attr !== "lastName"}
-										<li>{attr}: {val}</li>
-									{/if}
-								{/each}
-							</ul>
-						</details>
-					{/each}
+				{#if loadDataLoading}
+				<button type="button" class="secondary" aria-busy={queryLoading ? "true" : "false"}>Please wait...</button>
 				{:else}
-					<p>{resultsText}</p>
+				<button type="button" class="secondary" on:click={onLoad}
+				disabled={(loadDataLoading || clearDataLoading || queryLoading) ? true : false}>Load Data</button>
 				{/if}
-			</output>
-		</div>
-	</form>
+				{#if clearDataLoading}
+				<button type="reset" class="destructive" aria-busy={queryLoading ? "true" : "false"}>Please wait...</button>
+				{:else}
+					<button type="reset" class="destructive" disabled={clearDisabled} on:click={onClear}
+						>Clear Data</button>
+				{/if}
+			</div>
+			<fieldset>
+				<div class="grid">
+					<label>
+						First Name
+						<input type="text" placeholder="Enter a first name" bind:value={firstName} />
+					</label>
+					<label>
+						Last Name
+						<input type="text" placeholder="Enter a last name" bind:value={lastName} />
+					</label>
+				</div>
+				<small>At least one field must be entered</small>
+				{#if queryLoading}
+				<button type="submit" aria-busy={queryLoading ? "true" : "false"}>Please wait...</button>
+				{:else}
+				<button type="submit" on:click={onSubmit} disabled={(loadDataLoading || clearDataLoading || queryLoading) ? true : false}>Query</button>
+				{/if}
+			</fieldset>
+			<div class="container">
+				<h3>Results</h3>
+				<output>
+					{#if resultsText.length > 0}
+						{#each JSON.parse(resultsText).users as user}
+							<details>
+								<summary>
+									{user.firstName} {user.lastName}
+								</summary>
+								<ul>
+									{#each Object.entries(user) as [attr, val]}
+										{#if attr !== "firstName" && attr !== "lastName"}
+											<li>{attr}: {val}</li>
+										{/if}
+									{/each}
+								</ul>
+							</details>
+						{/each}
+					{:else}
+						<p>{resultsText}</p>
+					{/if}
+				</output>
+			</div>
+		</form>
+	</article>
 </main>
