1f0f749353f38d82b5d5cfdb12f14fd44d39df07
Neil Wiborg <NeilWiborg@outlook.com>
2023-05-03 22:44:34 -0700

    setup route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { defaultRoute } from './defaultRoute';
 import { dataRoute } from './program4/dataRoute';
+import { PeerReviewsRoute } from './PeerReviewsRoute';
 
 export const routes = express.Router();
 
 routes.use(defaultRoute);
 routes.use(dataRoute);
+routes.use(PeerReviewsRoute);
