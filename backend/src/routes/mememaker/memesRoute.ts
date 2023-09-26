39258404d78e9d94c0e7379ba40b3aa2c6759b4d
Neil Wiborg <NeilWiborg@outlook.com>
2023-09-26 18:04:15 -0500

    add get meme route

--- backend/src/routes/mememaker/memesRoute.ts
+++ backend/src/routes/mememaker/memesRoute.ts
@@ -3,44 +3,54 @@ import express from 'express';
 export const memesRoute = express.Router();
 
 type meme = {
-	id: string;
-	name: string;
-	url: string;
-	width: number;
-	height: number;
-	box_count: number;
-	captions: number;
+        id: string;
+        name: string;
+        url: string;
+        width: number;
+        height: number;
+        box_count: number;
+        captions: number;
 };
 
 type memeResponse = {
-	data: {memes: meme[]};
+        data: {memes: meme[]};
+};
+
+const getImgflipMeme = async (url: string) => {
+	let res = await fetch(url);
+	return res.blob();
 };
 
 const top100Memes = async () => {
-	let res = await fetch("https://api.imgflip.com/get_memes");
-	return res;
+        let res = await fetch("https://api.imgflip.com/get_memes");
+        return res.json();
 };
 
 const searchMemes = async (query: string) => {
-	let topMemesRes = await top100Memes();
-	let topMemes = await topMemesRes.json();
-	let res: memeResponse = {data: {memes: []}};
+        let topMemes = await top100Memes();
+        let res: memeResponse = {data: {memes: []}};
 
-	for (const m of topMemes.data) {
-		if (m.name.includes(query)) {
-			res.data.memes.push(m);
-		}
-	}
-	return res;
+        for (const m of topMemes.data.memes) {
+                if (m.name.toLowerCase().includes(query.toLowerCase())) {
+                        res.data.memes.push(m);
+                }
+        }
+        return res;
 };
 
+memesRoute.get('/mememaker/meme', async (req, res, next) => {
+	let url = req.query.url as string;
+	let resp = await getImgflipMeme(url);
+	res.send(resp);
+});
+
 memesRoute.get('/mememaker/top100', async (req, res, next) => {
-	let resp = await top100Memes();
-	return resp;
+        let resp = await top100Memes();
+        res.send(resp);
 });
 
 memesRoute.get('/mememaker/searchmemes', async (req, res, next) => {
-	let searchterm = req.query.searchterm as string;
-	let resp = await searchMemes(searchterm);
-	return resp;
+        let searchterm = req.query.searchterm as string;
+        let resp = await searchMemes(searchterm);
+        res.send(resp);
 });
