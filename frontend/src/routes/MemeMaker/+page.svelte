454d0111301cd02b6e831e26abc6bfa559db4ff5
Neil Wiborg <NeilWiborg@outlook.com>
2023-09-26 19:49:13 -0500

    add text to image

--- frontend/src/routes/MemeMaker/+page.svelte
+++ frontend/src/routes/MemeMaker/+page.svelte
@@ -15,41 +15,35 @@
 	});
 
 	const loadBackground = (background: File) => {
-		if (mounted) {
+		if (mounted && templateFabricCanvas === undefined) {
 			templateFabricCanvas = new fabric.Canvas(templateCanvas!);
 			templateFabricCanvas.selection = false;
 			fabric.Image.fromURL(URL.createObjectURL(background), function(oImg) {
-				// oImg.set("selectable", false);
 				templateFabricCanvas!.setBackgroundImage(oImg, () => {
 					templateFabricCanvas!.setWidth(oImg.getScaledWidth());
 					templateFabricCanvas!.setHeight(oImg.getScaledHeight());
 					templateFabricCanvas!.renderAll();
 					downloadURL = templateFabricCanvas!.toDataURL({format: "jpeg"});
 				});
-			// templateFabricCanvas!.add(oImg);
 			});
-			// const ctx = templateCanvas!.getContext('2d')!;
-			// const templateBackground = new Image();
-			// templateBackground.onload = () => {
-			// 	canvasWidth = templateBackground.width;
-			// 	canvasHeight = templateBackground.height;
-			// 	ctx.drawImage(templateBackground, 0, 0);
-			// };
-			// templateBackground.src = URL.createObjectURL(background);
-			// downloadURL = templateCanvas!.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
 		}
+	};
+
+	const addTextbox = () => {
+		let textbox = new fabric.Textbox("Enter text", {
+			fontFamily: 'Impact',
+			fill: "white",
+			stroke: "black",
+			width: 100,
+			editable: true
+		});
+
+		templateFabricCanvas?.add(textbox);
 	};
 
 	$: if (templates) {
 		loadBackground(templates[0]);
 	}
-
-	const downloadMeme = () => {
-		if (mounted) {
-			return templateCanvas!.toDataURL("image/png");
-		}
-		return "";
-	};
 </script>
 
 <svelte:head>
@@ -59,14 +53,21 @@
 <main class="container">
 	<article>
 		<h2>Meme Maker</h2>
-		<form>
-			<input type="file" id="template" name="template" accept="image/*" bind:files={templates}>
-		</form>
 		{#if !templates}
+			<form>
+				<input type="file" id="template" name="template" accept="image/*" bind:files={templates}>
+			</form>
 			<p>Please upload a meme template.</p>
 		{/if}
-		<canvas bind:this={templateCanvas} width=0 height=0>
-		</canvas>
-		<a role="button" href={downloadURL} download="image.jpeg">Download meme</a>
+		<div class="grid">
+			<canvas bind:this={templateCanvas} width=0 height=0 hidden={!templates}/>
+			{#if templates}
+				<div>
+					<button>Add image...</button>
+					<button on:click={() => addTextbox()}>Add textbox</button>
+					<a role="button" href={downloadURL} download="image.jpeg">Download meme</a>
+				</div>
+			{/if}
+		</div>
 	</article>
 </main>
