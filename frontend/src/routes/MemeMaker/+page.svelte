6262738198b9f30b2c8e9e053522b5cf636f7fbe
Neil Wiborg <NeilWiborg@outlook.com>
2023-09-29 21:30:45 -0500

    add templates page and improve meme maker

--- frontend/src/routes/MemeMaker/+page.svelte
+++ frontend/src/routes/MemeMaker/+page.svelte
@@ -2,12 +2,20 @@
 	import { fabric } from "fabric";
 	import { onMount } from "svelte";
 
+	type canvasObjects = {
+		images: fabric.Image[],
+		textboxes: fabric.Textbox[]
+	}
+
 	let mounted = false;
 	let templates: FileList | undefined = undefined;
 	let templateCanvas: HTMLCanvasElement | undefined = undefined;
 	let templateFabricCanvas: fabric.Canvas | undefined = undefined;
-	let topText = "";
-	let bottomText = "";
+	let fabricObjects: canvasObjects = {
+		images: [],
+		textboxes: []
+	}
+	let fontSize = 20;
 	let downloadURL =  "";
 
 	onMount(() => {
@@ -18,12 +26,14 @@
 		if (mounted && templateFabricCanvas === undefined) {
 			templateFabricCanvas = new fabric.Canvas(templateCanvas!);
 			templateFabricCanvas.selection = false;
+			// templateFabricCanvas.on("after:render", (e) => {
+			// 	downloadURL = templateFabricCanvas!.toDataURL({format: "jpeg"});
+			// });
 			fabric.Image.fromURL(URL.createObjectURL(background), function(oImg) {
 				templateFabricCanvas!.setBackgroundImage(oImg, () => {
 					templateFabricCanvas!.setWidth(oImg.getScaledWidth());
 					templateFabricCanvas!.setHeight(oImg.getScaledHeight());
 					templateFabricCanvas!.renderAll();
-					downloadURL = templateFabricCanvas!.toDataURL({format: "jpeg"});
 				});
 			});
 		}
@@ -32,14 +42,23 @@
 	const addTextbox = () => {
 		let textbox = new fabric.Textbox("Enter text", {
 			fontFamily: 'Impact',
+			fontSize: fontSize,
 			fill: "white",
 			stroke: "black",
 			width: 100,
 			editable: true
 		});
+		fabricObjects.textboxes.push(textbox);
 
-		templateFabricCanvas?.add(textbox);
+		templateFabricCanvas!.add(textbox);
 	};
+
+	const changeFontSize = () => {
+		fabricObjects.textboxes.forEach((item, index) => {
+			item.fontSize = fontSize;
+		});
+		templateFabricCanvas?.renderAll();
+	}
 
 	$: if (templates) {
 		loadBackground(templates[0]);
@@ -63,9 +82,15 @@
 			<canvas bind:this={templateCanvas} width=0 height=0 hidden={!templates}/>
 			{#if templates}
 				<div>
-					<button>Add image...</button>
-					<button on:click={() => addTextbox()}>Add textbox</button>
-					<a role="button" href={downloadURL} download="image.jpeg">Download meme</a>
+					<form>
+						<label>
+							Font size
+							<input type="text" bind:value={fontSize} on:input={changeFontSize} />
+						</label>
+						<button>Add image</button>
+						<button on:click={() => addTextbox()}>Add textbox</button>
+						<a role="button" href={downloadURL} download="image.jpeg">Download meme</a>
+					</form>
 				</div>
 			{/if}
 		</div>
---
+++ frontend/src/routes/MemeMaker/templates/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	type meme = {
+        id: string;
+        name: string;
+        url: string;
+        width: number;
+        height: number;
+        box_count: number;
+        captions: number;
+	};
+
+	type memeResponse = {
+        data: {memes: meme[]};
+	};
+
+	let loading = true;
+	let searchQuery = "";
+	let memeResults: meme[] = [];
+
+	onMount(async () => {
+		const res = await fetch(import.meta.env.VITE_BACKEND_HOSTNAME + "/mememaker/top100");
+		memeResults = (await res.json() as memeResponse).data.memes;
+		loading = false;
+	});
+
+	const onSearch = () => {
+		loading = true;
+		fetch(import.meta.env.VITE_BACKEND_HOSTNAME + "/mememaker/searchmemes?" + new URLSearchParams({
+			searchterm: searchQuery
+		})).then((response) => response.json()
+		.then((responseData: memeResponse) => {
+			memeResults = responseData.data.memes;
+		}));
+		loading = false;
+	};
+</script>
+
+<svelte:head>
+	<title>Meme Maker Templates</title>
+</svelte:head>
+
+<main class="container">
+	<article>
+		<h2>Meme Maker Templates</h2>
+		<form>
+			<input type="search" placeholder="Search templates..." bind:value={searchQuery} />
+			<button type="submit" on:click={onSearch}>Search Templates</button>
+		</form>
+		{#if loading}
+		<p>Loading...</p>
+		{:else}
+			{#each memeResults as meme}
+				<article>
+					<a href={"/MemeMaker?" + new URLSearchParams({
+						templateUrl: encodeURIComponent(meme.url)
+					})}><img src={meme.url} alt={meme.name}></a>
+					<a href={"/MemeMaker?" + new URLSearchParams({
+						templateUrl: encodeURIComponent(meme.url)
+					})}><h3>{meme.name}</h3></a>
+				</article>
+			{/each}
+		{/if}
+	</article>
+</main>
