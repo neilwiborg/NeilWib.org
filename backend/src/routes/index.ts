8c0a9072918f4566a50eda7dc178476f7a1e0625
Neil Wiborg <NeilWiborg@outlook.com>
2024-01-11 16:29:53 -0600

    remove css436 backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { defaultRoute } from './defaultRoute';
-import { dataRoute } from './program4/dataRoute';
 import { memesRoute } from './mememaker/memesRoute';
 import { memeScraperRoute } from './mememaker/memeScraperRoute';
 import { PeerReviewsRoute } from './PeerReviewsRoute';
@@ -8,7 +7,6 @@ import { PeerReviewsRoute } from './PeerReviewsRoute';
 export const routes = express.Router();
 
 routes.use(defaultRoute);
-routes.use(dataRoute);
 routes.use(memesRoute);
 routes.use(memeScraperRoute);
 routes.use(PeerReviewsRoute);
