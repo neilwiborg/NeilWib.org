4e56b0509d674a295f5588bccd97969f268c040f
Neil Wiborg <NeilWiborg@outlook.com>
2025-03-17 22:17:55 -0500

    migrate to server.ts (#11)

---
+++ backend/src/server.ts
@@ -0,0 +1,54 @@
+import http from 'http';
+import app from './app';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+const normalizePort = (val: string) => {
+	const port = parseInt(val, 10);
+	return isNaN(port) ? val : port >= 0 ? port : false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+const onError = (error: NodeJS.ErrnoException) => {
+	if (error.syscall !== 'listen') throw error;
+	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+	switch (error.code) {
+		case 'EACCES':
+			console.error(`${bind} requires elevated privileges`);
+			process.exit(1);
+		case 'EADDRINUSE':
+			console.error(`${bind} is already in use`);
+			process.exit(1);
+		default:
+			throw error;
+	}
+};
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+const onListening = () => {
+	const addr = server.address();
+	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+	console.log(`Listening on ${bind}`);
+};
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
