f958c203bca81ede2b729f2cbead566785e90112
Neil Wiborg <NeilWiborg@outlook.com>
2026-03-14 23:55:31 -0700

    use dev dependency

--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -1,17 +1,7 @@
 import pino from "pino";
 
-const isDevelopment = process.env.NODE_ENV !== "production";
-
 export const logger = pino({
-	level: process.env.LOG_LEVEL ?? (isDevelopment ? "debug" : "info"),
-	transport: isDevelopment
-		? {
-				target: "pino-pretty",
-				options: {
-					colorize: true,
-					ignore: "pid,hostname",
-					translateTime: "SYS:standard",
-				},
-			}
-		: undefined,
+	level:
+		process.env.LOG_LEVEL ??
+		(process.env.NODE_ENV !== "production" ? "debug" : "info"),
 });
